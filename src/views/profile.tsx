5cf83488fa0c664c01e81ec1bc5dd7e1ef438f25
hussainkazemian <hussain.kazemian@metropolia.fi>
2025-02-25 13:40:15 +0200

    Add Profile screen component

---
+++ src/views/profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+type Props = {}
+
+const profile = (props: Props) => {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  )
+}
+
+export default profile
